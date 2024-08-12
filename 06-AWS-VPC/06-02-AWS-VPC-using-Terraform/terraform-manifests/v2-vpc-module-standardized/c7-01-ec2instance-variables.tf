cc134ca36534f7500ea4f6d4eba87b93eb0f52d6
vibhavari saraf <vibhavarisaraf4555@gmail.com>
2024-08-12 18:07:04 +0530

    vpc and ec2 and security-group added

--- 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c7-01-ec2instance-variables.tf
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c7-01-ec2instance-variables.tf
@@ -8,5 +8,5 @@ variable "instance_type" {
 variable "instance_keypair" {
   description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
   type = string
-  default = "mandar-verginia"
+  default = "my-private-1"
 }
